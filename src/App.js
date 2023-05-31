9e6cda68aea14313d49e95f790bb5b0badebdd70
S-hee-sung <dlsglrhdwjs@naver.com>
2023-05-31 20:07:05 +0900

    2차 페이지 수정중 05.31

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
+import AboutPage from "./components/pages/AboutPage";
 
 function App() {
   return (
     <BrowserRouter> 
       <Routes>
         <Route exact path="/" element={<Home />} />
-
+        <Route exact path="/about" element={<AboutPage />} />
+        
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/pages/AboutPage.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from "styled-components";
+
+import PageWrap from '../../organize/templates/PageWrap';
+import Heading from '../../assets/Heading';
+import Header from '../../organize/Header';
+
+const AboutWrapper = styled.main`
+	section {
+		max-width: 1140px;
+		margin: auto;
+		padding-bottom: 80px;
+		&:not(:last-child) {
+			position: relative;
+			margin-bottom: 60px;
+			&::after {
+				content: "";
+				position: absolute;
+				bottom: 0;
+				left: 0;
+				right: 0;
+				margin: auto;
+				width: 500px;
+				height: 1px;
+				background-color: #252525;
+			}
+		}
+		> h2 {
+			margin-bottom: 40px;
+			font-size: 26px;
+			font-weight: 600;
+			text-align: center;
+		}
+	}
+	@media ${(props) => props.theme.mobile} {
+		section {
+			padding-bottom: 40px;
+			&:not(:last-child) {
+				&::after {
+					width: 70%;
+				}
+			}
+		}
+	}
+`;
+
+
+function AboutPage(props) {
+
+  return (
+    <PageWrap page="about">
+      <AboutWrapper>
+        <Header titleBg="Skills">
+          About <span className="point">Me</span>
+        </Header>
+
+        <section>
+          <Heading level="2">MY SKILLS</Heading>
+        </section>
+
+      </AboutWrapper>
+    </PageWrap>
+    );
+}
+
+export default AboutPage;
